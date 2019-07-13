254610a8713a36b8b82a8f411257f99ce5543061
Kyle Oba <koba@mudphone.com>
2019-07-12 22:41:42 -1000

    Add notes

--- lib/lelepua.ex
+++ lib/lelepua.ex
@@ -15,14 +15,22 @@ defmodule Lelepua do
 
   """
   defmacro left ~>> right do
+    # Assuming: 2 ~>> div(4)
     [{h, _}|t] = Macro.unpipe({:|>, [], [left, right]})
+    # => [{2, 0}, {{:div, [line: 3], [4]}, 0}]
+    
+    # :lists.foldl is like: reduce(fn, initial_val, list)
+    # consuming list from left to right (lowest to highest index)
     :lists.foldl fn
+      # first clause matches fn calls
       {{_, _, args} = x, _pos}, acc ->
+        # pos is one index higher than the last arg
         pos = Enum.count(args)
-        Macro.pipe(acc, x, pos)
+      Macro.pipe(acc, x, pos)
+
+      # second clause matches terms (like an integer)
       {x, pos}, acc ->
         Macro.pipe(acc, x, pos)
     end, h, t
   end
-
 end
